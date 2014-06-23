b3bd2287bb746c0b704734a27150cf4fb00f7097
RobertoHE <rober.herrera92@hotmail.com>
2014-06-23 10:53:59 +0200

    trim

--- trunk/code/sandbox/trim/trim.ino
+++ trunk/code/sandbox/trim/trim.ino
@@ -3,8 +3,10 @@
 int port[Nmotor]={
   32,31,24,44,25,45,23,43,30,33,12,6,26,7,10,4,11,5,8,2,9,3};
   
-
-  
+//enum configuracion {nouso=-1,NeckY=0,HipY,RShoulderR,LShoulderR,RShoulderP,LShoulderP,
+//                   RElbowP,LElbowP,RLegY,LLegY,RLegR,LLegR,RLegP,LLegP,RKnee1,LKnee1,RKnee2,LKnee2,RAnkleP,
+//                   LAnkleP,RAnkleR,LAnkleR,NeckP};
+//  
   
 /*   Trim sin carga
 int trim[Nmotor]={
@@ -12,9 +14,7 @@ int trim[Nmotor]={
 */  
 /*TRIM CARGA*/
 int trim[Nmotor]={
-  -6,-9,3,-7,6,0,54,-44,-3,-5,32,9,3,-6,-2,-4,-2,2,2,0,-5,2};
-  
-  
+  -6,-9,3,-7,6,0,54,-44,-7,-3,13,11,13,-7,1,0,-5,0,0,5,10,0};
 int vector_1[22] = {
     90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90};
 
